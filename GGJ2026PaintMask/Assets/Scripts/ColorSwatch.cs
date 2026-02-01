f90f6ab1ed499b58dfcd00779c05559cfd56d392
LudicWill <williamnhoffmann@gmail.com>
2026-02-01 13:45:49 -0800

    added random color swatch generation with some rules (current generates 3 colors: red, blue, green)

--- GGJ2026PaintMask/Assets/Scripts/ColorSwatch.cs
+++ GGJ2026PaintMask/Assets/Scripts/ColorSwatch.cs
@@ -9,5 +9,62 @@ namespace GGJ2026.Painting
         public Color TAPE;
         public Color BLACK;
         public Color WHITE;
+
+        public bool randomRGBColorScheme;
+
+        public ToolSelectButton randomPaintButtonRed;
+        public ToolSelectButton randomPaintButtonBlue;
+        public ToolSelectButton randomPaintButtonGreen;
+
+        public float hueRedMin;
+        public float hueRedMax;
+        public float hueGreenMin;
+        public float hueGreenMax;
+        public float hueBlueMin;
+        public float hueBlueMax;
+        public float valueMin;
+        public float valueMax;
+        public float saturationMin;
+        public float saturationMax;
+
+        public Color randomRed;
+        public Color randomBlue;
+        public Color randomGreen;
+
+
+
+        void Start()
+        {
+            if (randomRGBColorScheme)
+            {
+                SetRandomRGBColorScheme();
+                SetRandomButtonColors();
+            }
+        }
+
+        void SetRandomRGBColorScheme()
+        {
+            float hueRed = Random.Range(hueRedMin, hueRedMax);
+            float hueBlue = Random.Range(hueBlueMin, hueBlueMax);
+            float hueGreen = Random.Range(hueGreenMin, hueGreenMax);
+
+            float saturation = Random.Range(saturationMin, saturationMax);
+            float value = Random.Range(valueMin, valueMax);
+
+            randomRed = Color.HSVToRGB(hueRed, saturation, value);
+            randomBlue = Color.HSVToRGB(hueBlue, saturation, value);
+            randomGreen = Color.HSVToRGB(hueGreen, saturation, value);
+        }
+
+        void SetRandomButtonColors()
+        {
+            randomPaintButtonRed.SetColor(randomRed);
+            randomPaintButtonBlue.SetColor(randomBlue);
+            randomPaintButtonGreen.SetColor(randomGreen);  
+        }
+
+        
+
+        
     }
 }
